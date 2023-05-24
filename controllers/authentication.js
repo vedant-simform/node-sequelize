993d50b60c20b8bdfb8101ce5ffc193579b939a0
vedant-simform <vedant@simformsolutions.com>
2023-05-24 19:05:39 +0530

    backend-integration

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,8 @@
-const { sequelize } = require("../models");
-const bcrypt = require("bcrypt");
+const { sequelize } = require('../models');
+const bcrypt = require('bcrypt');
 const authentication = sequelize.models.authentication;
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 async function login(req, res) {
   try {
@@ -12,16 +12,16 @@ async function login(req, res) {
     });
 
     if (!logindata) {
-      res.status(404).json({ message: "No Record Found" });
+      res.status(404).json({ message: 'No Record Found' });
     }
 
     const status = await bcrypt.compare(passwd, logindata.passwd);
     if (status) {
       const token = jwt.sign({ userId: logindata.id }, process.env.JWT_SECRET);
 
-      res.status(200).json({ message: "Authentication successful", token });
+      res.status(200).json({ message: 'Authentication successful', token });
     } else {
-      res.status(401).json({ message: "Authentication Failed" });
+      res.status(401).json({ message: 'Authentication Failed' });
     }
   } catch (Error) {
     res.json({ Error });
@@ -33,7 +33,7 @@ async function signup(req, res) {
     let { username, passwd } = req.body;
     passwd = await bcrypt.hash(passwd, 10);
     await authentication.create({ username, passwd });
-    res.status(200).json({ message: "Data Entry Successfull" });
+    res.status(200).json({ message: 'Data Entry Successfull' });
   } catch (error) {
     console.log(error);
   }
--- start.js
+++ start.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const authenticationRouter = require("./Routes/authentication");
-const crud = require("./Routes/crud");
-require("dotenv").config();
+const express = require('express');
+const authenticationRouter = require('./Routes/authentication');
+const crud = require('./Routes/crud');
+const metaToken = require('./Routes/metaToken');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
 app.use(authenticationRouter);
 app.use(crud);
+app.use(metaToken);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
